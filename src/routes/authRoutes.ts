f577abde6f716c2941fabb887da47229c31fe1da
vishnu <getvishnuvs@gmail.com>
2026-03-29 20:52:52 +0530

    feat:feature/user/auth

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import  express from "express";
 import { authController } from "../di/auth.js";
+import { AuthMiddleware } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 router.post("/signup",authController.signup)
 router.post("/login",authController.login)
+router.post("/logout",AuthMiddleware.authenticate,authController.logout)
 
 
 
